a9193edbb0b050db0bfde41f8449e2e92060c5d8
Joshua Colley <>
2018-08-11 10:14:44 +0100

    add list interactor test for: - displayError - isLoading

--- Marvelicious/MarveliciousTests/Character List Tests/MVLCharacterListInteractorTests.swift
+++ Marvelicious/MarveliciousTests/Character List Tests/MVLCharacterListInteractorTests.swift
@@ -34,21 +34,47 @@ class MVLCharacterListInteractorTests: XCTestCase {
         sut.getCharacters(offset: offset)
         
         // Then
+        XCTAssertTrue(viewSpy.didStartLoading)
         XCTAssertTrue(viewSpy.didCallDisplayCharacters)
+        XCTAssertTrue(viewSpy.didEndLoading)
+    }
+    
+    func testDidCallDisplayError() {
+        // Given
+        let offset = 0
+        networkSpy.shouldFail = true
+        
+        // When
+        sut.getCharacters(offset: offset)
+        
+        // Then
+        XCTAssertTrue(viewSpy.didStartLoading)
+        XCTAssertTrue(viewSpy.didCallDisplayError)
+        XCTAssertTrue(viewSpy.didEndLoading)
     }
 }
 
 class MVLCharacterListViewSpy: MVLCharacterListViewProtocol {
     var didCallDisplayCharacters: Bool = false
+    var didCallDisplayError: Bool = false
+    var didStartLoading: Bool = false
+    var didEndLoading: Bool = false
     
     func displayCharacters(_ models: [MVLCharacter]) {
         didCallDisplayCharacters = true
     }
     
     func displayError(message: String) {
+        didCallDisplayError = true
+        
     }
     
     func isLoading(_ isLoading: Bool) {
+        if isLoading {
+            didStartLoading = true
+        } else {
+            didEndLoading = true
+        }
     }
 }
 
